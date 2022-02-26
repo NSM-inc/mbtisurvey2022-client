2290561b4dce76c97f3bbcca50232f520d9bffca
imzeze <jihyekim019@gmail.com>
2022-02-26 21:47:45 +0900

    feat: layout 폴더 생성

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,6 +1,3 @@
-import Header from '../layout/Header';
-import Footer from '../layout/Footer';
-import ContentsContainer from '../layout/ContentsContainer';
 import Image from './Image';
 
-export { Header, Footer, ContentsContainer, Image };
+export { Image };
