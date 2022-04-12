da67feb8859af1eb006b377e14865f93b7a460a8
imzeze <jihyekim019@gmail.com>
2022-04-13 01:37:49 +0900

    Feat: ENV API Call 로직 개발

--- src/pages/api/firebase.ts
+++ src/pages/api/firebase.ts
@@ -1,18 +1,12 @@
-import { initializeApp } from 'firebase/app';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export const useFirebaseAuth = () => {
-    const firebaseConfig = {
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    res.status(200).json({
         apiKey: process.env.apiKey,
         authDomain: process.env.authDomain,
         projectId: process.env.projectId,
         storageBucket: process.env.storageBucket,
         messagingSenderId: process.env.messagingSenderId,
         appId: process.env.appId,
-    };
-
-    const app = initializeApp(firebaseConfig);
-
-    return {
-        app,
-    };
+    });
 };
